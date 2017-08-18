31093e2699260a3228ebb8b04c33168b6ec0e6ee
karurus <wuttipichai.karunarat@g.swu.ac.th>
2017-08-18 22:43:35 +0700

    Update CafeController Authorize

--- Controllers/CafeController.cs
+++ Controllers/CafeController.cs
@@ -1,23 +1,21 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using poppy.cat.Models;
+using System; 
+using System.Collections.Generic; 
+using System.Diagnostics; 
+using System.Linq; 
+using System.Threading.Tasks; 
+using Microsoft.AspNetCore.Mvc; 
+using Microsoft.AspNetCore.Authorization; 
+using poppy.cat.Models; 
 
-namespace poppy.cat.Controllers
-{
-    public class CafeController : Controller
-    {
-        public ActionResult Cafe()
-        {
-            return View();
+namespace poppy.cat.Controllers {
+    
+    public class CafeController:Controller {
+        public ActionResult Cafe() {
+            return View(); 
         }
-
-        public ActionResult CataholicCafe()
-        {
-            return View();
+        [Authorize] 
+        public ActionResult CataholicCafe() {
+            return View(); 
         }
     }
 }
