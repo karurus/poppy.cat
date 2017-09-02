14abb23d6d9e673bb27736fbc0f95b0b041895b5
wuttipichai karunarat <wu_tipichai555@hotmail.com>
2017-09-02 16:31:43 +0700

    Update db

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using poppy.cat.Models;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
 
 namespace poppy.cat.Controllers
 {
@@ -14,7 +12,28 @@ namespace poppy.cat.Controllers
         {
             return View();
         }
-
+        public IActionResult Index1()
+        {
+            SqlConnection conn = new SqlConnection("Server=tcp:poppycat.database.windows.net,1433;Initial Catalog=PoppyCat;Persist Security Info=False;User ID=chikarurus;Password=Karunarat1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM CatCafe", conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<CatCafe> catcafes = new List<CatCafe>();
+            while (reader.Read())
+            {
+                CatCafe cafe = new CatCafe();
+                cafe.ID = reader.GetInt32(reader.GetOrdinal("ID"));
+                cafe.Name = reader.GetString(reader.GetOrdinal("Name"));
+                cafe.Price = reader.GetInt32(reader.GetOrdinal("Price"));
+                cafe.RateStar = reader.GetInt32(reader.GetOrdinal("RateStar"));
+                cafe.RateNum = reader.GetInt32(reader.GetOrdinal("RateNum"));
+                //cafe.Img = reader.GetString(reader.GetOrdinal("Img"));
+                //cafe.Place = reader.GetString(reader.GetOrdinal("Place"));
+                catcafes.Add(cafe);
+            }
+            ViewBag.catcafes = catcafes;
+            return View();
+        }
         public IActionResult About()
         {
             return View();
---
+++ Models/CatCafe.cs
@@ -0,0 +1,12 @@
+namespace poppy.cat.Models { 
+public class CatCafe{
+
+    public int ID;
+    public string Name;
+    public int Price;
+    public int RateStar;
+    public int RateNum;
+    public string Img;
+    public string Place;
+}
+}
